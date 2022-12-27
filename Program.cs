e7f8260fc7f2f8cd1668ba285acc188537ba5bab
Viktoriia Nekrasova <vvinebrand@gmail.com>
2022-12-28 00:54:46 +0300

    Изм. второй задачи

--- Program.cs
+++ Program.cs
@@ -26,14 +26,33 @@ while (begin)
 
         case 2:
 
-            int row = ReadInt("Введите индекс строки: ");
-            int columns = ReadInt("Введите индекс столбца: ");
-            int[,] num = new int[6, 8];
-            FillArray2D(num);
-            PrintArray2D(num);
-
-            if (row < num.GetLength(0) && columns < num.GetLength(1)) Console.WriteLine(num[row, columns]);
-            else Console.WriteLine($"{row}{columns} -> такого числа в массиве нет");
+            Console.Write("Введите строку: ");
+            int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+            Console.Write("Введите столбец: ");
+            int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+            int n = 5;
+            int m = 7;
+            Random random = new Random();
+            int[,] arr = new int[n, m];
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(10, 99);
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+            {
+                Console.WriteLine("Элемент не существует");
+            }
+            else
+            {
+                Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+            }
+            Console.ReadLine();
 
             break;
 
@@ -72,34 +91,4 @@ void PrintArray(double[,] array)
         Console.Write("]");
         Console.WriteLine("");
     }
-}
-
-void FillArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-
-void PrintArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
 }
