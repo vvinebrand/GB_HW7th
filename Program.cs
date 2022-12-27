d9751cecddebdc57f844d462701adf95ef2f3258
Viktoriia Nekrasova <vvinebrand@gmail.com>
2022-12-28 00:39:42 +0300

    Скелет программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+int programm, num;
+Boolean begin = true;
+
+while (begin)
+{
+    Console.WriteLine("\nЗадача 1. Двумерный массив, со случайными вещественными числами\nЗадача 2. Нахождение максимального из трех значений\nЗадача 3. Определение четности/нечетности\nЗадача 4. Вывод всех четных чисел от 1 до N\nВведите 0 для выхода\n");
+    Console.Write("Введите номер задачи: ");
+    programm = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    switch (programm)
+    {
+        case 1:
+
+           
+            break;
+
+        case 2:
+
+           
+            break;
+
+        case 3:
+
+           
+            break;
+
+        case 4:
+
+            
+            break;
+
+        default:
+            begin = false;
+            break;
+    }
+}
